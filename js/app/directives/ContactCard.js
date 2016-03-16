67c13b7c0f12011074d10b8fb62eb70a208d6c7a
onyoo <roberto9m9@gmail.com>
2016-03-16 15:38:04 +0100

    refactor to directive

--- js/app/directives/ContactCard.js
+++ js/app/directives/ContactCard.js
@@ -2,7 +2,11 @@ function ContactCard() {
   return {
     template: [
             '<div class="contact">',
+            '<h4>Contact Card</h4>',
               '<a href="https://twitter.com/{{contact.name}}">Follow @{{contact.name}} on twitter!</a>',
+              '<label>Name: {{contact.name}}</label>',
+              '<label>Email: {{contact.email}}</label>',
+              '<label>Phone: {{contact.phone}}</label>',
             '</div>'
         ].join(''),
         restrict: 'E',
