9a0b23e2f680cef7a5027e79d8b8e609445be4b1
Adam King <adam.king0126@gmail.com>
2016-04-06 17:41:05 -0500

    Done.

---
+++ js/app/directives/ContactCard.js
@@ -0,0 +1,17 @@
+function ContactCard() {
+  return {
+    template: [
+      '<div class="contact">',
+      '<h4>Contact Card</h4>',
+      '<label>Name: </label>{{ contact.name }}',
+      '<label>Email: </label>{{ contact.email }}',
+      '<label>Phone: </label>{{ contact.phone }}',
+      '</div>'
+    ].join(''),
+    restrict: 'E'
+  };
+}
+
+angular
+  .module('app')
+  .directive('contactCard', ContactCard);
