e18844a97051cb2d428c6fa87dc8324351e8d6ce
c1505 <coreym247@gmail.com>
2016-05-07 17:11:11 -0400

    work in progress

---
+++ js/app/directives/ContactCard.js
@@ -0,0 +1,21 @@
+function ContactCard() {
+  return {
+    template: [
+    "<div>",
+      "<h4>Contact Card</h4>",
+      "<label>Name:{{contact.name}}</label>",
+      "<label>Email:{{contact.email}}</label>",
+      "<label>Phone:{{contact.phone}}</label>",
+    "</div>"
+    ].join('')
+    // scope: {
+    //     name: '=',
+    //     email: '=',
+    //     phone: '='
+    // }
+  }
+}
+
+angular
+  .module('app')
+  .directive('contactCard', ContactCard)
