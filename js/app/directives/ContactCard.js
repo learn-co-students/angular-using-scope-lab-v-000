7ae219a6c6ef355ee91deb95299a55d1a7288fce
febbraiod <donedesign1@gmail.com>
2016-05-12 12:59:57 -0400

    Done.

---
+++ js/app/directives/ContactCard.js
@@ -0,0 +1,20 @@
+function ContactCard(){
+  return {
+    template:['<div>',
+    '<h4>Contact Card</h4>',
+    '<label>Name:</label>',
+    '{{contact.name}}',
+    '<label>Email:</label>',
+    '{{contact.email}}',
+    '<label>Phone:</label>',
+    '{{contact.phone}}',
+    '</div>'
+    ].join(''),
+    scope: { contact: '=' }
+  };
+}
+
+
+angular
+  .module('app')
+  .directive('contactCard', ContactCard);
