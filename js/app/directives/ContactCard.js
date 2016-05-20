b2c4e1afdfccdf07f18724fa8dd1db7e95478365
shinyrachel <rachel.kory@gmail.com>
2016-05-20 16:30:56 -0400

    complete

---
+++ js/app/directives/ContactCard.js
@@ -0,0 +1,19 @@
+function ContactCard() {
+  return {
+    template: [
+      '<div>',
+        '<h4>Contact Card</h4>',
+        '<label>Name: {{ contact.name }}</label>',
+        '<label>Email: {{ contact.email }}</label>',
+        '<label>Phone: {{ contact.phone }}</label>',
+      '</div>'
+    ].join(''),
+    scope: {
+      contact: '='
+    }
+  };
+}
+
+angular
+  .module('app')
+  .directive('contactCard', ContactCard)
