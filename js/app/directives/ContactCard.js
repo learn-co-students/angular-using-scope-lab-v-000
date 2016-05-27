e3831afdb1ae21be61605b3977f9166950d98ee7
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-05-26 19:34:22 -0500

    Done.

---
+++ js/app/directives/ContactCard.js
@@ -0,0 +1,20 @@
+function ContactCard() {
+  return {
+    template: [
+      "<div>",
+        "<h4>Contact Card</h4>",
+        "<label>Name: {{ contact.name }} </label>",
+        "<label>Email: {{ contact.email }} </label>",
+        "<label>Phone: {{ contact.phone }} </label>",
+      "</div>"
+    ].join(''),
+    .scope: {
+      handle: @
+    }
+    .restrict: "E"
+  };
+}
+
+angular
+  .module('app')
+  .directive('contactCard', contactCard)
