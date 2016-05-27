449cb5026d83e5c636f572602406a6a36905f27f
Enoch Griffith <enoch2k2@hotmail.com>
2016-05-26 23:41:55 -0500

    Done.

---
+++ js/app/directives/ContactCard.js
@@ -0,0 +1,20 @@
+function ContactCard(){
+  return {
+    template: [
+      '<div>',
+        '<h4>Contact Card</h4>',
+        '<label>Name: </label>',
+        '<p>{{ contact.name }}</p>',
+        '<label>Email: </label>',
+        '<p>{{ contact.email }}</p>',
+        '<label>Phone: </label>',
+        '<p>{{ contact.phone }}</p>',
+      '</div>'
+    ].join(''),
+    restrict: 'E'
+  };
+}
+
+angular
+  .module('app')
+  .directive('contactCard', ContactCard);
