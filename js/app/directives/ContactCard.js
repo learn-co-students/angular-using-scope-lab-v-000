92e0794c6889bce9dbe7d28025926b3d26c44f07
the-widget <brennenawana108ny@gmail.com>
2016-06-24 09:37:04 -0400

    One solution, but not using scope

---
+++ js/app/directives/ContactCard.js
@@ -0,0 +1,15 @@
+function ContactCard(){
+  return {
+    template: ['<div>',
+     '<h4>Contact Card</h4>',
+      '<label>Name: {{contact.name}}</label>',
+       '<label class="email">Email: {{contact.email}}</label>',
+        '<label>Phone: {{contact.phone}}</label>',
+         '</div></br>'].join(''),
+    restrict: 'E'
+  };
+};
+
+angular
+  .module('app')
+  .directive('contactCard', ContactCard);
