9ae5627d25f8003aa701d21a49a0454a5ef7b03b
Peter Jacobson <github email address>
2016-08-16 14:29:12 -0400

    Done.

---
+++ js/app/directives/ContactCard.js
@@ -0,0 +1,23 @@
+function ContactCard() {
+return {
+  scope: {
+    name: '=',
+    email: '=',
+    phone: '='
+  },
+template: [
+'<div>,
+    <h4>Contact Card</h4>,
+          '<label>NAME: {{ handle.name }}</label>',
+        '<label>EMAIL: {{ handle.email }}</label>',
+        '<label>Phone: {{ handle.phone }}</label>',
+ 
+</div>'
+].join(''),
+restrict: 'E'
+};
+}
+ 
+angular
+.module('app')
+.directive('contactCard', ContactCard);
