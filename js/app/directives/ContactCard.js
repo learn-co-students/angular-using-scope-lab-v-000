5a2b58003c1690403dbe264fe849641d854b6396
jd2rogers2 <jd2rogers2@gmail.com>
2016-11-01 10:54:13 -0400

    donzo

---
+++ js/app/directives/ContactCard.js
@@ -0,0 +1,21 @@
+function ContactCard(){
+  return {
+    scope: {
+      name: '@',
+      email: '@',
+      phone: '@'
+    },
+    template: [
+      "<div>",
+        "<h4>Contact Card</h4>",
+        "<label>Name:</label>{{name}}",
+        "<label>Email:</label>{{email}}",
+        "<label>Phone:</label>{{phone}}",
+      "</div>",
+    ].join('')
+  }
+}
+
+angular
+  .module('app')
+  .directive('contactCard', ContactCard);
