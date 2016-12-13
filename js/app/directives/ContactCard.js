13ee88fba847768fe85a279c9f4bd7d39e7ca5f0
jbok4 <jbok4@aol.com>
2016-12-13 08:07:14 -0500

    complete

--- js/app/directives/ContactCard.js
+++ js/app/directives/ContactCard.js
@@ -1,25 +1,23 @@
-function ContactCard() {
-    return {
-        template: [
-            '<div>',
-                '<h4>Contact Card</h4>',
-                '<label>Name:</label>',
-                '<p>{{ handle.name }}</p>'
-                '<label>Email:</label>',
-                '<p>{{ handle.email }}</p>'
-                '<label>Phone:</label>',
-                '<p>{{ handle.phone }}</p>'
-            '</div>'
-        ].join(''),
-        restrict: 'E',
-        scope: {
-          handle: '='
-        }
-    };
+function ContactCard(){
+  return {
+    template: [
+      '<div>',
+        '<h4>Contact Card</h4>',
+        '<label>Name: </label>',
+        '<p>{{ handle.name }}</p>',
+        '<label>Email: </label>',
+        '<p>{{ handle.email }}</p>',
+        '<label>Phone: </label>',
+        '<p>{{ handle.phone }}</p>',
+      '</div>'
+    ].join(''),
+    restrict: 'E',
+    scope: {
+      handle: '='
+    }
+  };
 }
- 
+
 angular
-    .module('app')
-    .directive('contactCard', ContactCard);
-
-
+  .module('app')
+  .directive('contactCard', ContactCard);
