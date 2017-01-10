36cbc1c857c97eb7dc12b613de4fe271c9f86d98
Christian Whitesides <whitesidescr@gmail.com>
2017-01-10 15:18:52 -0700

    Done.

---
+++ js/app/directives/ContactCard.js
@@ -0,0 +1,20 @@
+function ContactCard() {
+  return {
+    template: [
+      '<div>',
+        '<h4>Contact Card</h4>',
+        '<label>Name:</label>',
+        '{{ contact.name }}',
+        '<label>Email:</label>',
+        '{{ contact.email }}',
+        '<label>Phone:</label>',
+        '{{ contact.phone }}',
+      '</div>'
+    ].join(''),
+    restrict: 'E'
+  };
+}
+
+angular
+  .module('app')
+  .directive('contactCard', ContactCard);
