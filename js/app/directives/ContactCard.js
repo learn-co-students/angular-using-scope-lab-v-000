b43e12c7862c415d9cd463cc90b59a42e8e07d57
Chris Bruen <bruen.chris@gmail.com>
2017-01-16 22:19:46 -0500

    Done.

---
+++ js/app/directives/ContactCard.js
@@ -0,0 +1,17 @@
+function ContactCard() {
+  return {
+    template: ['<div>', '<h4>Contact Card</h4>',
+   '<label>Name:</label> {{name}}', '<label>Email:</label> {{email}}',
+   '<label>Phone:</label> {{phone}}', '</div>'].join(''),
+   restrict: 'E',
+   scope: {
+     name: '@',
+     email: '@',
+     phone: '@'
+   }
+ };
+}
+
+angular
+.module('app')
+.directive('contactCard', ContactCard);
